b77273cdf900defd87aa958d5575956781a918c5
Dmitry Bukin <bukind@indoaustinvestments.com>
2023-11-25 18:04:56 +1100

    Create helper to build the app

---
+++ 09wa/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This is a simple helper to build the app.
+# Run it from its directory such as:
+#
+#   ./build.sh
+#
+
+[ -d assets ] || (echo 'assets subdir is not found'; exit 1)
+
+GOOS=js GOARCH=wasm go build -v -n -x -o assets/main.wasm cmd/wasm/main.go
+cp -v -u "$(go env GOROOT)/misc/wasm/wasm_exec.js" assets/
